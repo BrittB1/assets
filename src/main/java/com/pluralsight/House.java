010f56788cb3f465decaa146085c75ddd1e4b3bc
Brittany Buchanan <bbuchanan0400@gmail.com>
2025-10-28 11:59:28 -0400

    Initial commit. Created super class Asset and child class House w/ constructors, super call, getters and setters.

---
+++ src/main/java/com/pluralsight/House.java
@@ -0,0 +1,65 @@
+package com.pluralsight;
+
+public class House extends Asset {
+
+    String address;
+    int condition, squareFoot, lotSize;
+
+    public House(String description, String dateAcquired,String address,double originalCost,
+                 int condition, int squareFoot, int lotSize ) {
+        super(description, dateAcquired, originalCost);
+
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+    @Override
+    public double getValue() {
+        double pricePerSqFt = switch (condition) {
+            case 1 -> 180.0;
+            case 2 -> 130.0;
+            case 3 ->  90.0;
+            case 4 ->  80.0;
+            default -> 0.0;
+        };
+
+        return (pricePerSqFt * squareFoot) + (lotSize * 0.25);
+    }
+
+    }
+
+
+
